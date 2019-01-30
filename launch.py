107cb472fbb10dd3f0b01dd9c3b458af8c83a87c
Vegard Joerg <vegard1992@gmail.com>
2019-01-30 03:02:48 +0100

    .

--- launch.py
+++ launch.py
@@ -1,44 +1,14 @@
-def do_git(command, cwd):
-    import subprocess
-    cmd = [command.split(" ")]
-    p = subprocess.Popen(cmd, cwd=cwd)
-    p.wait()
 
 def launch():
     from time import sleep
 
     import os 
-    #os.popen("mkdocs build")
+    os.popen("mkdocs build")
 
-    cwd = os.getcwd()
-    temp_folder = "/".join(cwd.split("\\")[:-2]) + "/temp"
+    # now copy /site to ../getagripdocs
 
-    import shutil
-    shutil.rmtree(temp_folder)
-    #os.mkdir(temp_folder)
-    shutil.copytree("site", temp_folder)
-
-    from dulwich import porcelain
-
-    #git_lines_1 = [
-    #    "git checkout gh-pages"
-    #]
-
-    #git_lines_2 = [
-    #    "git commit -m '.'",
-    #    "git push -u origin gh-pages",
-    #    "git checkout -b master"
-    #]
-
-    #for gl in git_lines_1:
-    #    do_git(gl, cwd)
-
-
-    #shutil.rmtree(".")
-    #shutil.copytree(temp_folder+"/temp", ".")
-
-    #for gl in git_lines_2:
-    #    do_git(gl, cwd)
+    # and commit+push both repos
+    # using dulwich porcelain
 
 
 
